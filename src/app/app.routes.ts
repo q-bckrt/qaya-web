94d2dc680046b27fa1c8500e48d4b0710969b426
q-bckrt <pom.schc@egmail.com>
2025-07-06 14:45:20 +0200

    setup: basic project structure and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import {ShellComponent} from './layout/shell/shell.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import {AccountsComponent} from './features/accounts/accounts.component';
+import {TransactionsComponent} from './features/transactions/transactions.component';
+import { AccountDetailsComponent } from './features/account-details/account-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'accounts', component: AccountsComponent },
+      { path: 'accounts/:id', component: AccountDetailsComponent },
+      { path: 'transactions', component: TransactionsComponent },
+
+    ]
+  }
+];
