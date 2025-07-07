a70d0e9a46a55fe8dfbcf9c9b8d28197dd2cb6ef
q-bckrt <pom.schc@egmail.com>
2025-07-07 23:21:44 +0200

    added mock charts on dashboard

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -3,7 +3,10 @@ import {TransactionsService} from '../../services/transactions.service';
 import {TransactionDto} from '../../models/transaction.dto';
 import {NgClass, NgForOf} from '@angular/common';
 import { CurrencyIntlPipe} from '../../shared/pipes/currency-intl.pipe';
-import {NgChartsModule} from 'ng2-charts';
+import { NgChartsModule } from 'ng2-charts';
+import { ChartConfiguration } from 'chart.js/auto';
+import { ChartData, ChartOptions, ChartType, ChartDataset } from 'chart.js';
+
 
 
 @Component({
@@ -12,10 +15,11 @@ import {NgChartsModule} from 'ng2-charts';
     NgForOf,
     NgClass,
     CurrencyIntlPipe,
-    NgChartsModule
+    NgChartsModule,
   ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
+  standalone: true
 })
 export class DashboardComponent implements OnInit {
 
@@ -32,4 +36,67 @@ export class DashboardComponent implements OnInit {
       }
     });
   }
+
+  totalSparklineData: ChartData<'line', number[], string> = {
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    datasets: [
+      {
+        label: 'This Week',
+        data: [1200, 1100, 1300, 1250, 1400, 1350, 1420],
+        fill: false,
+        borderColor: '#4ADE80',
+        tension: 0.4,
+        pointRadius: 0,
+        borderWidth: 2
+      } as ChartDataset<'line', number[]>
+    ]
+  };
+
+  incomeSparklineData: ChartData<'line', number[], string> = {
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    datasets: [
+      {
+        label: 'This Week',
+        data: [3000, 3200, 3100, 2800, 2700, 2500, 2300],
+        fill: false,
+        borderColor: '#60A5FA', // blue-400
+        tension: 0.4,
+        pointRadius: 0,
+        borderWidth: 2
+      } as ChartDataset<'line', number[]>
+    ]
+  };
+
+  expenseSparklineData: ChartData<'line', number[], string> = {
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    datasets: [
+      {
+        label: 'This Week',
+        data: [900, 1050, 1300, 1250, 1400, 1500, 1560],
+        fill: false,
+        borderColor: '#F472B6', // pink-400
+        tension: 0.4,
+        pointRadius: 0,
+        borderWidth: 2
+      } as ChartDataset<'line', number[]>
+    ]
+  };
+
+  sparklineOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: { display: false },
+      tooltip: { enabled: false }
+    },
+    scales: {
+      x: { display: false },
+      y: { display: false }
+    },
+    elements: {
+      line: { borderJoinStyle: 'round' },
+      point: { radius: 0 }
+    }
+  };
+
 }
